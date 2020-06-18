d4be142a9ca3b192c371cec184891b06fd389ba3
ben_coelho <ben_coelho@hotmail.com>
2020-06-18 10:05:11 +1000

    working api queries

--- Assets/script.js
+++ Assets/script.js
@@ -1,54 +1,77 @@
 $(document).ready(function () {
+  function buildGeoCodeURL() {
+    var queryURLGeo = "https://maps.googleapis.com/maps/api/geocode/json?";
 
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
-  
+    var queryParams = { key: "AIzaSyAwmiVLmIUNhiWqaGiGzlHl7WIec1ST8Ys" };
 
-// api.openweathermap.org/data/2.5/weather?appid=c8a65159cdc7e9fd6ca2d010f396a049&q=Melbourne,VIC,Australia
+    queryParams.address = $("#search-term").val().trim();
 
+    console.log("---------------\nURL: " + queryURLGeo + "\n---------------");
+    console.log(queryURLGeo + $.param(queryParams));
 
+    return queryURLGeo + $.param(queryParams);
+  }
+  // var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
 
-    // Set the API key
-    var queryParams = { "appid": "c8a65159cdc7e9fd6ca2d010f396a049" };
+  function weatherURL(lat, lng) {
+    console.log(lat);
+    console.log(lng);
 
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?";
 
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
+    // lat={lat}&lon={lon}&exclude={part}&appid={YOUR API KEY}
+
+    var queryParams = { appid: "c8a65159cdc7e9fd6ca2d010f396a049" };
+
+    queryParams.lat = lat;
+    queryParams.lon = lng;
+
     return queryURL + $.param(queryParams);
   }
-  
 
-  
-
-  $("#run-search").on("click", function(event) {
+  $("#run-search").on("click", function (event) {
     // This line allows us to take advantage of the HTML "submit" property
     // This way we can hit enter on the keyboard and it registers the search
     // (in addition to clicks). Prevents the page from reloading on form submit.
     event.preventDefault();
-  
 
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  console.log(queryURL)
+    var queryURLGeo = buildGeoCodeURL();
+    console.log(queryURLGeo);
     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
     // The data then gets passed as an argument to the updatePage function
 
+    $.ajax({
+      url: queryURLGeo,
+      method: "GET",
+    }).then(function (responseGeo) {
+      var lat = responseGeo.results[0].geometry.location.lat;
+      var lng = responseGeo.results[0].geometry.location.lng;
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response){
-    console.log(response);
+      var queryURL = weatherURL(lat, lng);
+      console.log(queryURL);
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+      });
+
+      // let unix_timestamp = 1549312452
+      // // Create a new JavaScript Date object based on the timestamp
+      // // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+      // var date = new Date(unix_timestamp * 1000);
+      // // Hours part from the timestamp
+      // var hours = date.getHours();
+      // // Minutes part from the timestamp
+      // var minutes = "0" + date.getMinutes();
+      // // Seconds part from the timestamp
+      // var seconds = "0" + date.getSeconds();
+
+      // var date=response.
+      // var temp=response.
+      // var rH=response.
+      // var windSpeed=response.
+      // var UV index=response.
+    });
   });
-
-  });
-
-
-
-})
+});
